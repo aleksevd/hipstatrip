587748fada24f3db22119c1376de8dba69f9aca3
aleksevd <aleks.evd@gmail.com>
2012-10-14 00:23:20 +0400

    gravatar added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar_image_tag(user)
+    default_url = "#{root_url}assets/guest.png"
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    image_tag("http://gravatar.com/avatar/#{gravatar_id}.png?s=108&d=#{CGI.escape(default_url)}")
+  end
 end
