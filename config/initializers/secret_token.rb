aea846facd16c9fed0cd808b63084a243b5b2705
Igor Sidorov <igor.cug@gmail.com>
2012-10-13 11:48:16 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HipstaTrip::Application.config.secret_token = 'a9d92e6664d9745a6174906c39b27f690ab68f87408944862911b314381cf06d25748d8cbc15e3b5d4830127729cb24543a558a76b0f1e4f56ae2c55c8d4c509'
