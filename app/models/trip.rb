6ba9b3e982de74c035fe84306a10d5d21de3e297
aleksevd <aleks.evd@gmail.com>
2012-10-13 17:04:19 +0400

    fix

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,6 +7,7 @@ class Trip < ActiveRecord::Base
   has_many :passengers_trips
   has_many :passengers, through: :passengers_trips, class_name: "User", foreign_key: :passenger_id
   has_many :comments, as: :owner
+  has_many :proposals
 
   accepts_nested_attributes_for :passengers_trips
 
