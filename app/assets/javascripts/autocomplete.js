be2342c1edf7cf632588d284e83ebf93523d97f6
aleksevd <aleks.evd@gmail.com>
2012-10-14 16:53:29 +0400

    geocomplete added

---
+++ app/assets/javascripts/autocomplete.js
@@ -0,0 +1,4 @@
+$(function(){
+    $("#trip_start_address").geocomplete();
+    $("#trip_end_address").geocomplete();
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ u.confirm!
 u2 = User.create!(email: "test2@example.com", password: "123123", password_confirmation: "123123")
 u2.confirm!
 
-t = Trip.create!(driver: u, starts_at: 1.day.since)
+t = Trip.create!(driver_id: u.id, starts_at: 1.day.since)
 
 Proposal.create!(sender: u2, receiver: u, trip: t)
