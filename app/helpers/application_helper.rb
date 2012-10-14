5b84f03b6af71230bd713d776b803fb95a4349a7
Igor Sidorov <igor.cug@gmail.com>
2012-10-14 23:03:08 +0400

    Added flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,17 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     image_tag("http://gravatar.com/avatar/#{gravatar_id}.png?s=108&d=#{CGI.escape(default_url)}")
   end
+
+  def flash_messages
+    [:notice, :alert, :warning].
+      select { |type| flash.key?(type) }.
+      map { |type| flash_block(type) }.
+      join
+  end
+
+private
+
+  def flash_block(type)
+    content_tag :div, flash[type], class: "flash flash_#{type}"
+  end
 end
