43ea6d19809a699332fb92f7b59be118f9e3f7b8
Igor Sidorov <igor.cug@gmail.com>
2012-10-14 13:31:20 +0400

    Added unicorn config

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,10 +30,12 @@ namespace :deploy do
   task :restart, except: { no_release: true } do
     run "kill -s USR2 `cat /tmp/unicorn.hipsta_trip.pid`"
   end
+
   task :link_database_config do
     run "ln -sF #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
 
+=begin
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
       from = source.next_revision(current_revision)
@@ -44,6 +46,7 @@ namespace :deploy do
       end
     end
   end
+=end
 end
 
 after "deploy:finalize_update", "deploy:link_database_config"
